ec65c9149dfa221d0fc7c6d84d20c4607c6c41fe
Ajeet <jeetay.yadav284@gmail.com>
2023-08-16 16:25:18 +0530

    changes in login and register page

--- pages/account/register.jsx
+++ pages/account/register.jsx
@@ -67,21 +67,21 @@ function Register() {
         <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-group">
                            
-                            <input name="firstName" placeholder ="Enter Name Here" type="text" {...register('firstName')} className={`form-control ${errors.firstName ? 'is-invalid' : ''}`} />
+                            <input name="firstName" placeholder ="First Name" type="text" {...register('firstName')} className={`form-control ${errors.firstName ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.firstName?.message}</div>
                         </div>
                         <div className="form-group">
                            
-                            <input name="lastName" placeholder ="Enter Company Name here" type="text" {...register('lastName')} className={`form-control ${errors.lastName ? 'is-invalid' : ''}`} />
+                            <input name="lastName" placeholder ="Last Name" type="text" {...register('lastName')} className={`form-control ${errors.lastName ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.lastName?.message}</div>
                         </div>
                         <div className="form-group">
                            
-                            <input name="username" placeholder ="Enter Email" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                            <input name="username" placeholder ="Username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.username?.message}</div>
                         </div>
 
-                        <div className="form-group">
+                       {/* <div className="form-group">
                             <select className='form-control'>
                                 <option>What Best Describes you ?</option>
                                 <option>Ecommerce</option>
@@ -93,23 +93,21 @@ function Register() {
 									<option>Other</option>
 
                             </select>
-                        </div>
+                        </div>*/ }
 
 
                         <div className="form-group">
-                           
-                            <input placeholder='Enter Password' name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                          
+                            <input placeholder ="Password" name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
                             <div className="invalid-feedback">{errors.password?.message}</div>
                         </div>
-
                         <div className="form-group">
-                           
                         <button disabled={formState.isSubmitting} className="btn btn-primary">
                             {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
                             Register
                         </button>
                         </div>
-                        <p class="avc">Alread have account ? please <Link href="/account/login" className="lnac">login</Link></p>
+                        <p class="avc">Don't have account ? please <Link href="/account/login" className="lnac"> Login</Link></p>
                     </form>
                     
       </div>
@@ -119,7 +117,38 @@ function Register() {
 </section>
 
 
-            
+            <div className="card">
+                <h4 className="card-header">Register</h4>
+                <div className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <label>First Name</label>
+                            <input name="firstName" type="text" {...register('firstName')} className={`form-control ${errors.firstName ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.firstName?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Last Name</label>
+                            <input name="lastName" type="text" {...register('lastName')} className={`form-control ${errors.lastName ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.lastName?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input name="username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </div>
+                        <button disabled={formState.isSubmitting} className="btn btn-primary">
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                            Register
+                        </button>
+                        <Link href="/account/login" className="btn btn-link">Cancel</Link>
+                    </form>
+                </div>
+            </div>
         </Layout>
     );
 }
